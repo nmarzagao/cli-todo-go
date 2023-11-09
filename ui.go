5ac5b241136836d619b62c45e0dc34d653e475a1
nmarzagao <nicolas.marzagao@gmail.com>
2023-11-09 11:36:26 -0300

    Ui Overhall

--- ui.go
+++ ui.go
@@ -4,18 +4,58 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 )
 
 const (
+	// COLORS
 	RESET  = "\033[0m"
 	RED    = "\033[91m"
 	GREEN  = "\033[92m"
 	YELLOW = "\033[33m"
+
+	// CONTROLLS
+	W_KEY     = 119
+	UP_KEY    = 65
+	S_KEY     = 115
+	DOWN_KEY  = 66
+	ENTER_KEY = 10
+	SPACE_KEY = 32
+	Q_KEY 	  = 113
 )
 
+func disable_CtrC() {
+	// Handle interrupt signal (Ctrl+C)
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigc
+		// Handle interrupt (Ctrl+C) here if necessary
+		// For example, you can print a message instead of breaking the loop
+		fmt.Println("Ctrl+C is disabled in this program.")
+	}()
+}
+
+func clear_screen() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func disable_terminal() {
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+    exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+}
+
+func enable_terminal() {
+	exec.Command("stty", "-F", "/dev/tty", "echo").Run()
+}
+
+
 func print_list(list *List, cur_pos int) {
+	fmt.Println(GREEN + "\t\tTODO LIST" + RESET + "\n")
+
 	if list.is_empty() {
-		return
+		fmt.Println("List is Empty!\n")
 	}
 
 	tmp := list.head
@@ -42,39 +82,37 @@ func print_list(list *List, cur_pos int) {
 func menu(list *List) {
 	cur_pos := 1
 	var input []byte = make([]byte, 1)
+	exit := false
 
-	// disable terminal for input
-	// fix ctr C because it breaks the program
-	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-    exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+	disable_terminal()
+	disable_CtrC()
     
-    for {
-		cmd := exec.Command("clear") 
-        cmd.Stdout = os.Stdout
-        cmd.Run()
-
-		fmt.Println(GREEN + "\t\tTODO LIST" + RESET + "\n")
+    for !exit {
+		clear_screen()
 		print_list(list, cur_pos)
 
-		fmt.Println(input)
-		fmt.Println(cur_pos)
-
 		os.Stdin.Read(input)
-		if input[0] == 119 || input[0] == 65 { // up
+
+		switch input[0] {
+		case W_KEY, UP_KEY:
 			if cur_pos > 1 {
 				cur_pos -= 1
 			}
-		} else if input[0] == 115 || input[0] == 66 { // down
+		
+		case S_KEY, DOWN_KEY:
 			if cur_pos < list.size {
 				cur_pos += 1
 			}
-		} else if input[0] == 32 || input[0] == 10 {
+		
+		case ENTER_KEY, SPACE_KEY:
 			list.set_task(cur_pos)
-		} else if input[0] == 113 {
-			break
+		
+		case Q_KEY:
+			exit = true
+		
+		default:
+			continue
 		}
-
     }
-
-	exec.Command("stty", "-F", "/dev/tty", "echo").Run()
+	enable_terminal()
 }
